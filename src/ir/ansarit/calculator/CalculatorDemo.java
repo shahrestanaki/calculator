171930de1c371f0e57d5b743da174ff7ed1f67d4
mohammad <41846836+shahrestanaki@users.noreply.github.com>
2019-12-12 15:34:46 +0330

    Update CalculatorDemo.java
    1- شکستن متد اصلی به متدهای ریزتر باعث خوانایی بهتر و همچنین مدیریت بهتر روی روند آن می شود. ضمن آنکه برای اعمال تغییرات عمده کار را راحت تر هم می کند
    2- زمان ایجاد آبجت هایی که مرتب استفاده می شود و یک لوپ هم داریم بهتر است در بیرون لوپ تعریف شوند تا بارها و بارها ساخته نشوند یا به آن ها حافظه اختصاص داده نشود
    inputUserType و Scanner
    3- باز هم بررسی کنید شاید شما بهتر توانستید بنویسید
    موفق باشید

--- src/ir/ansarit/calculator/CalculatorDemo.java
+++ src/ir/ansarit/calculator/CalculatorDemo.java
@@ -7,49 +7,59 @@ import java.util.Scanner;
 
 public class CalculatorDemo {
     public static void main(String[] args) {
-        boolean wantToUse = true;
-        do{
-            System.out.println("Calculator is ready to use :)");
-            Scanner scanner = new Scanner(System.in);
-            ArithmeticOperation calculator = new Calculator();
-            ArrayList<String> blockList = calculator.extractTokens(String.valueOf(scanner.next()));
-            Double result = null;
-            switch (blockList.get(2)){
-                case "+":{
-                    result = calculator.add(Double.parseDouble(blockList.get(0)),Double.parseDouble(blockList.get(1)));
-                }
-                break;
-                case "-":{
-                    result = calculator.subtract(Double.parseDouble(blockList.get(0)),Double.parseDouble(blockList.get(1)));
-                }
-                break;
-                case "*":{
-                    result = calculator.multiplication(Double.parseDouble(blockList.get(0)),Double.parseDouble(blockList.get(1)));
-                }
-                break;
-                case "/":{
-                    try {
-                        result = calculator.division(Double.parseDouble(blockList.get(0)),Double.parseDouble(blockList.get(1)));
-                    }
-                    catch (Exception e){
-                        System.out.println("division By zero occurred");
-                    }
-                }
-                break;
-                default:{
-                    System.out.println("invalid input! smile");
-                }
+        boolean wantToUse;
+        Scanner scanner = new Scanner(System.in);
+        String inputUserType;
+        do {
+            inputUserType = getInputUserdata(scanner, true);
+            Double result = calc(inputUserType);
+            if (result != null) {
+                System.out.println(result);
             }
-            System.out.println(result);
-            System.out.println("continue? y/n");
-            if ("y".equals(String.valueOf(scanner.next()))){
-                wantToUse = true ;
-            }
-            else {
+            inputUserType = getInputUserdata(scanner, false);
+            if ("y".equals(inputUserType)) {
+                wantToUse = true;
+            } else {
                 wantToUse = false;
             }
-        }while(wantToUse);
 
+        } while (wantToUse);
+    }
 
+    static String getInputUserdata(Scanner scanner, boolean flag) {
+        if (flag) {
+            System.out.println("Calculator is ready to use :)");
+        } else {
+            System.out.println("continue? y/n");
+        }
+        String inputUserType = String.valueOf(scanner.next());
+        return inputUserType;
+    }
+
+    static Double calc(String inputUserType) {
+        ArithmeticOperation calculator = new Calculator();
+        ArrayList<String> blockList = calculator.extractTokens(inputUserType);
+        switch (blockList.get(2)) {
+            case "+": {
+                return calculator.add(Double.parseDouble(blockList.get(0)), Double.parseDouble(blockList.get(1)));
+            }
+            case "-": {
+                return calculator.subtract(Double.parseDouble(blockList.get(0)), Double.parseDouble(blockList.get(1)));
+            }
+            case "*": {
+                return calculator.multiplication(Double.parseDouble(blockList.get(0)), Double.parseDouble(blockList.get(1)));
+            }
+            case "/": {
+                try {
+                    return calculator.division(Double.parseDouble(blockList.get(0)), Double.parseDouble(blockList.get(1)));
+                } catch (Exception e) {
+                    System.out.println("division By zero occurred");
+                }
+            }
+            default: {
+                System.out.println("invalid input! smile");
+            }
+        }
+        return null;
     }
 }
